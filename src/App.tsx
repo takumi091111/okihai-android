f1b62aca9a422118785806603672dfd6a3267c9c
takumi091111 <takumi091111@gmail.com>
2019-10-15 14:57:50 +0900

    プロジェクトの追加

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const style = StyleSheet.flatten({
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center'
+})
+
+export default () => (
+  <View style={style}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+  </View>
+)
