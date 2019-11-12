fdf042536bf9c559c004cfb43a7f0dda8a8dcea1
takumi091111 <takumi091111@gmail.com>
2019-11-12 16:53:36 +0900

    ロック画面のテキストを実装

--- src/screens/Lock.tsx
+++ src/screens/Lock.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { TouchableWithoutFeedback, StyleSheet } from 'react-native'
+import { Text } from 'react-native-elements'
 import { AnimatedCircularProgress } from 'react-native-circular-progress'
 import Icon from 'react-native-vector-icons/Feather'
 import { NavigationContainerProps, withNavigation } from 'react-navigation'
@@ -7,13 +8,19 @@ import Container from '@/components/Container'
 import Header from '@/components/Header'
 import { toggleLock } from '@/utils/api'
 
-const iconStyle = StyleSheet.flatten({
-  top: -150
+const styles = StyleSheet.create({
+  icon: {
+    top: -150
+  },
+  text: {
+    top: -50,
+    fontSize: 24,
+    fontWeight: '600'
+  }
 })
 
 const Lock = ({ navigation }: NavigationContainerProps) => {
   const [isLocked, setIsLocked] = useState(true)
-  const [lockButtonText, setLockButtonText] = useState('Unlock')
   const [progress, setProgress] = useState(0)
   const [isPressIn, setIsPressIn] = useState(false)
   const [isToggled, setIsToggled] = useState(false)
@@ -64,7 +71,6 @@ const Lock = ({ navigation }: NavigationContainerProps) => {
           fill={progress}
           tintColor='#000'
           duration={0}
-          lineCap='round'
         />
         {
           <TouchableWithoutFeedback
@@ -74,10 +80,13 @@ const Lock = ({ navigation }: NavigationContainerProps) => {
             <Icon
               name={isLocked ? 'lock' : 'unlock'}
               size={100}
-              style={iconStyle}
+              style={styles.icon}
             />
           </TouchableWithoutFeedback>
         }
+        <Text style={styles.text}>
+          {isLocked ? 'LOCKED' : 'UNLOCKED'}
+        </Text>
       </Container>
     </>
   )
