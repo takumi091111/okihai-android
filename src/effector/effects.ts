ca4ca15b4f1e41ca29be48f786fea14fee586eb4
takumi091111 <takumi091111@gmail.com>
2020-01-14 15:14:53 +0900

    通知トークンがセットされないバグを修正

--- src/effector/effects.ts
+++ src/effector/effects.ts
@@ -1,5 +1,17 @@
 import { createEffect } from 'effector'
 import { AsyncStorage } from 'react-native'
+import { registerForPushNotification } from '@/utils/notification'
+
+export const fetchNoticeToken = createEffect({
+  handler: async () => {
+    const result = await registerForPushNotification()
+    if (result.ok === true) {
+      return result.data.token
+    } else {
+      return null
+    }
+  }
+})
 
 export const fetchTokenFromAsyncStorage = createEffect({
   handler: async () => {
--- src/effector/stores/noticeToken.ts
+++ src/effector/stores/noticeToken.ts
@@ -2,8 +2,13 @@ import { createStore } from 'effector'
 
 import { UpdateNoticeToken } from '@/effector/events'
 import { NoticeToken } from '@/interfaces/State'
+import { fetchNoticeToken } from '@/effector/effects'
 
-export const noticeToken = createStore<NoticeToken>(null).on(
+export const noticeToken = createStore<NoticeToken>(null)
+.on(fetchNoticeToken.done, (_state, { result }) => result)
+.on(
   UpdateNoticeToken,
   (_state, noticeToken) => noticeToken
 )
+
+fetchNoticeToken()
