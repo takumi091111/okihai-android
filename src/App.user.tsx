99108843affabb6daf8e0e0fff4720708e44b692
takumi091111 <takumi091111@gmail.com>
2020-01-31 02:45:05 +0900

    通知処理の修正

--- src/App.user.tsx
+++ src/App.user.tsx
@@ -2,20 +2,13 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { ToastAndroid } from 'react-native'
 import { ThemeProvider } from 'react-native-elements'
 
-import { UpdateNoticeToken, UpdateUser } from '@/effector/events'
+import { UpdateUser } from '@/effector/events'
 import createAppContainer from '@/navigations/user'
 import { loggedIn } from '@/utils/api/user'
-import { useNotification } from '@/utils/hooks/useNotification'
 import { useSplashScreen } from '@/utils/hooks/useSplashScreen'
 import theme from '@/utils/theme'
 
 const App = () => {
-  const {
-    token,
-    isDenied,
-    hasPermission,
-    askPermissionWithToken
-  } = useNotification()
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const { isReady, hide } = useSplashScreen({
     waitSec: 1000,
@@ -35,23 +28,8 @@ const App = () => {
     hide()
   }
 
-  const askNoticeToken = async () => {
-    await askPermissionWithToken()
-    if (isDenied) return
-    UpdateNoticeToken(token)
-  }
-
-  const updateNoticeToken = async () => {
-    if (hasPermission) {
-      UpdateNoticeToken(token)
-      return
-    }
-    askNoticeToken()
-  }
-
   useEffect(
     useCallback(() => {
-      updateNoticeToken()
       hideSplashAfterLoggedIn()
     }, []),
     []
